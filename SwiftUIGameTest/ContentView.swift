64ec10a6aea842683c74e7fb7fcaecb18dd1602a
Edon <edon@valdman.works>
2024-03-22 13:48:17 -0500

    Initial Commit

---
+++ SwiftUIGameTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIGameTest
+//
+//  Created by Edon Valdman on 3/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIGameTest/SwiftUIGameTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGameTestApp.swift
+//  SwiftUIGameTest
+//
+//  Created by Edon Valdman on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGameTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
